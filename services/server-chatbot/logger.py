e5e61a8f46b3c74109668ff2c8c9e1a02a9cb7a6
Ng Le Jie <nglejie@gmail.com>
2026-07-08 15:16:55 +0800

    added function to not log if log_file env var is empty

--- services/server-chatbot/logger.py
+++ services/server-chatbot/logger.py
@@ -3,7 +3,7 @@ import os
 from logging.handlers import RotatingFileHandler
 
 LOG_LEVEL = os.getenv("LOG_LEVEL", "DEBUG")
-LOG_FILE = os.getenv("LOG_FILE", "/app/chatbot/chatbot.log")
+LOG_FILE = os.getenv("LOG_FILE", "")
 
 def get_logger(name : str) -> logging.Logger:
     """Define Logger for App logging
@@ -32,9 +32,10 @@ def get_logger(name : str) -> logging.Logger:
     logger.addHandler(console)
     
     # Rotating File handler to replace old logs
-    os.makedirs(os.path.dirname(LOG_FILE), exist_ok=True)
-    file_handler = RotatingFileHandler(LOG_FILE, maxBytes=10*1024*1024, backupCount=5)
-    file_handler.setFormatter(formatter)
-    logger.addHandler(file_handler)
+    if LOG_FILE:
+        os.makedirs(os.path.dirname(LOG_FILE), exist_ok=True)
+        file_handler = RotatingFileHandler(LOG_FILE, maxBytes=10*1024*1024, backupCount=5)
+        file_handler.setFormatter(formatter)
+        logger.addHandler(file_handler)
     
     return logger
